405f300082da42a6d7cbd5b7363f5d694fcf441a
jdosk <jdoskocil86@gmail.com>
2025-07-30 10:11:06 -0700

    Added index.html and script.js with initial code framework

---
+++ script.js
@@ -0,0 +1,25 @@
+// Function uses Math.random to return a random choice of rock, paper, scissors
+function getComputerChoice() {
+// TODO
+}
+
+// Function takes user input to get human choice of rock, paper, scissors
+function getHumanChoice() {
+// TODO
+// make sure its case insensitive
+}
+
+// Scores to keep track of whos winning over multiple rounds
+let humanScore = 0;
+let computerScore = 0;
+
+// Function to play a round - get both choices, then define the winner and add to score
+function playRound(getComputerChoice, getHumanChoice) {
+    // TODO
+}
+
+// Function to play a 5 round game
+function playGame() {
+    
+}
+
