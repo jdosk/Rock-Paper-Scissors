5a1d600eeec5ce697f5bb26c1c745a98a0f6cf82
jdosk <jdoskocil86@gmail.com>
2025-07-30 10:57:58 -0700

    Finished playRound()

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Function uses Math.random to return a random choice of rock, paper, scissors
 function getComputerChoice() {
-// TODO
     let choiceNum = Math.random() * 100;
     console.log(choiceNum);
     if (choiceNum > 60) {
@@ -23,7 +22,47 @@ let computerScore = 0;
 
 // Function to play a round - get both choices, then define the winner and add to score
 function playRound(getComputerChoice, getHumanChoice) {
-    // TODO
+    const humanChoice = getHumanChoice;
+    const computerChoice = getComputerChoice;
+
+    console.log("You chose " + humanChoice + " and the computer chose " + computerChoice);
+
+    switch (humanChoice) {
+        case "paper":
+            if (computerChoice == "paper") {
+                console.log("\nThis round is a Tie!\n");
+            } else if (computerChoice == "scissors") {
+                console.log("\nYou Lose this round!\n");
+                computerScore++;
+            } else {
+                console.log("\nYou Win this round!\n");
+                humanScore++;
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "scissors") {
+                console.log("\nThis round is a Tie!\n");
+            } else if (computerChoice == "rock") {
+                console.log("\nYou Lose this round!\n");
+                computerScore++;
+            } else {
+                console.log("\nYou Win this round!\n");
+                humanScore++;
+            }
+            break;
+        case "rock":
+            if (computerChoice == "rock") {
+                console.log("\nThis round is a Tie!\n");
+            } else if (computerChoice == "paper") {
+                console.log("\nYou Lose this round!\n");
+                computerScore++;
+            } else {
+                console.log("\nYou Win this round!\n");
+                humanScore++;
+            }
+            break;
+    }
+
 }
 
 // Function to play a 5 round game
@@ -33,4 +72,3 @@ function playGame() {
 
 
 /// FOR TESTING - DELETE ONCE NOT NEEDED
-getComputerChoice();
