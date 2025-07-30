0055a05425a22710683b504191975e65ab92a86b
jdosk <jdoskocil86@gmail.com>
2025-07-30 10:32:30 -0700

    Finished getComputerChoice() and getHumanChoice()

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Function uses Math.random to return a random choice of rock, paper, scissors
 function getComputerChoice() {
 // TODO
+    let choiceNum = Math.random() * 100;
+    console.log(choiceNum);
+    if (choiceNum > 60) {
+        return "rock";
+    } else if (choiceNum <= 30) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 // Function takes user input to get human choice of rock, paper, scissors
 function getHumanChoice() {
-// TODO
-// make sure its case insensitive
+    return prompt("Make your choice: Rock, Paper, Scissors").toLocaleLowerCase();
 }
 
 // Scores to keep track of whos winning over multiple rounds
@@ -20,6 +28,9 @@ function playRound(getComputerChoice, getHumanChoice) {
 
 // Function to play a 5 round game
 function playGame() {
-    
+    // TODO
 }
 
+
+/// FOR TESTING - DELETE ONCE NOT NEEDED
+getComputerChoice();
