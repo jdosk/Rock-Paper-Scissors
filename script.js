084cb82175b8ea682bcf24b00aecb323bd1d1aba
jdosk <jdoskocil86@gmail.com>
2025-08-03 11:34:21 -0700

    Added finished UI

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Global Variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Dom Selectors
+const buttons = document.querySelectorAll(".btn");
+const results = document.querySelector(".results");
+const playerScore = document.querySelector(".playerScore");
+const cpuScore = document.querySelector(".cpuScore");
+const outcome = document.querySelector(".outcome");
+
+
 // Function uses Math.random to return a random choice of rock, paper, scissors
 function getComputerChoice() {
     let choiceNum = Math.random() * 100;
@@ -13,83 +25,84 @@ function getComputerChoice() {
 }
 
 // Function takes user input to get human choice of rock, paper, scissors
-function getHumanChoice() {
-    return prompt("Make your choice: Rock, Paper, Scissors").toLocaleLowerCase();
+function getHumanChoice(e) {
+    return e.toLocaleLowerCase();
 }
 
+// Function to play a round - get both choices, then define the winner and add to score
+function playRound(getComputerChoice, getHumanChoice) {
+    const humanChoice = getHumanChoice;
+    const computerChoice = getComputerChoice;
 
+    // Display choices
+    results.textContent = "\nYou chose " + humanChoice + " and the computer chose " + computerChoice + ". ";
 
-// Function to play a 5 round game
-function playGame() {
-    let i = 0;
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // Function to play a round - get both choices, then define the winner and add to score
-    function playRound(getComputerChoice, getHumanChoice) {
-        const humanChoice = getHumanChoice;
-        const computerChoice = getComputerChoice;
-
-        console.log("\nYou chose " + humanChoice + " and the computer chose " + computerChoice);
-
-        // Switch to check computer choice against player choice and return correct score
-        switch (humanChoice) {
-            case "paper":
-                if (computerChoice == "paper") {
-                    console.log("\nThis round is a Tie!\n");
-                } else if (computerChoice == "scissors") {
-                    console.log("\nYou Lose this round!\n");
-                    computerScore++;
-                } else {
-                    console.log("\nYou Win this round!\n");
-                    humanScore++;
-                }
-                break;
-            case "scissors":
-                if (computerChoice == "scissors") {
-                    console.log("\nThis round is a Tie!\n");
-                } else if (computerChoice == "rock") {
-                    console.log("\nYou Lose this round!\n");
-                    computerScore++;
-                } else {
-                    console.log("\nYou Win this round!\n");
-                    humanScore++;
-                }
-                break;
-            case "rock":
-                if (computerChoice == "rock") {
-                    console.log("\nThis round is a Tie!\n");
-                } else if (computerChoice == "paper") {
-                    console.log("\nYou Lose this round!\n");
-                    computerScore++;
-                } else {
-                    console.log("\nYou Win this round!\n");
-                    humanScore++;
-                }
-                break;
-        }
-    }
-
-    // Game Begins Here
-    console.log("Welcome to Rock, Paper, Scissors!\n");
-
-    // While loop to iterate through the rounds
-    while (i < 5) {
-        playRound(getComputerChoice(), getHumanChoice());
-        console.log("The current score is\nComputer: " + computerScore +"\nPlayer: " + humanScore);
-        i++;
-    }
-
-    // Check scores and declare winner
-    if (computerScore > humanScore) {
-        console.log("Final Score:\nComputer: " + computerScore +"\nPlayer: " + humanScore +"\nGAME OVER - YOU LOSE");
-    } else if (humanScore > computerScore) {
-        console.log("Final Score:\nComputer: " + computerScore +"\nPlayer: " + humanScore +"\nGAME OVER - YOU WIN!!!");
-    } else {
-        console.log("Final Score:\nComputer: " + computerScore +"\nPlayer: " + humanScore +"\nGAME OVER - TIE");
+    // Switch to check computer choice against player choice, then edit & diisplay scores
+    switch (humanChoice) {
+        case "paper":
+            if (computerChoice == "paper") {
+                results.textContent += ("This round is a Tie!");
+            } else if (computerChoice == "scissors") {
+                results.textContent += ("You Lose this round!");
+                computerScore++;
+                cpuScore.textContent = "Computer: " + computerScore;
+            } else {
+                results.textContent += ("You Win this round!");
+                humanScore++;
+                playerScore.textContent = "Player: " + humanScore;
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "scissors") {
+                results.textContent += ("This round is a Tie!");
+            } else if (computerChoice == "rock") {
+                results.textContent += ("You Lose this round!");
+                computerScore++;
+                cpuScore.textContent = "Computer: " + computerScore;
+            } else {
+                results.textContent += ("You Win this round!");
+                humanScore++;
+                playerScore.textContent = "Player: " + humanScore;
+            }
+            break;
+        case "rock":
+            if (computerChoice == "rock") {
+                results.textContent += ("This round is a Tie!");
+            } else if (computerChoice == "paper") {
+                results.textContent += ("You Lose this round!");
+                computerScore++;
+                cpuScore.textContent = "Computer: " + computerScore;
+            } else {
+                results.textContent += ("You Win this round!");
+                humanScore++;
+                playerScore.textContent = "Player: " + humanScore;
+            }
+            break;
     }
 }
 
+// Event handler function, plays a round upon button click, then checks winner and 
+//      removes event listeners if a winner is declared
+function handleClick(event) {
+    playRound(getComputerChoice(), getHumanChoice(event.target.id));
+    if (checkWinner()) {
+        buttons.forEach((btn) => {
+            btn.removeEventListener("click", handleClick);
+        });
+    }           
+}
+// Add Event Handlers for the buttons
+buttons.forEach((button) => {
+    button.addEventListener("click", handleClick);
+});
 
-// Run the game
-playGame();
+// function to check if there is a winner. If a winner is found, return true and declare winner
+function checkWinner() {
+    if (humanScore == 5) {
+        outcome.textContent = "You WIN!!!! Congratulations!";
+        return true;
+    } else if (computerScore == 5) {
+        outcome.textContent = "You LOSE! Better luck next time."
+        return true;
+    } else return false;
+}
